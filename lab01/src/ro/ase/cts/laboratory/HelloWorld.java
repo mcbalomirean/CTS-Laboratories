4f807cbbd1b35175ca0261734df5deb84668a8e5
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2021-02-23 11:40:53 +0200

    Test a second commit.

--- lab01/src/ro/ase/cts/laboratory/HelloWorld.java
+++ lab01/src/ro/ase/cts/laboratory/HelloWorld.java
@@ -3,5 +3,6 @@ package ro.ase.cts.laboratory;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Test, World!");
     }
 }
