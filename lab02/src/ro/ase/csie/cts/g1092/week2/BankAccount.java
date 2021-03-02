23fcd6993a298154d52853b3a353f5da3601e2fa
Mircea-Cristian Balomirean <mcbalomirean@gmail.com>
2021-03-02 10:37:48 +0200

    Initial commit for laboratory 2.

---
+++ lab02/src/ro/ase/csie/cts/g1092/week2/BankAccount.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1092.week2;
+
+public abstract class BankAccount extends Account {
+    private String id;
+    private double balance;
+}
---
+++ lab02/src/ro/ase/csie/cts/g1092/week2/exceptions/IllegalTransferException.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1092.week2.exceptions;
+
+public class IllegalTransferException extends Exception {
+    public IllegalTransferException() {
+    }
+
+    public IllegalTransferException(String message) {
+        super(message);
+    }
+}
